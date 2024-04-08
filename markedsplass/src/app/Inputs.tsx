6f9b75bd1e864b356d93e51249e84674192c80bf
ValCordDev <88210628+ValCordDev@users.noreply.github.com>
2024-04-08 23:30:01 +0200

    installerte firebase

--- markedsplass/src/app/Inputs.tsx
+++ markedsplass/src/app/Inputs.tsx
@@ -1,42 +1,23 @@
-'use client';
+import React from 'react';
 
-import { signInWithEmailAndPassword } from 'firebase/auth';
-import React, { useState } from 'react';
-import { auth } from './firebase';
 
 const Inputs = () => {
-  const [email, setMail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const signIn = () => {
-    signInWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        console.log(userCredential);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
 
   return (
-    <form className="flex justify-center flex-col gap-5" onSubmit={signIn}>
+    <form className="flex justify-center flex-col gap-5">
       <input
         type="email" // Change "mail" to "email"
         className="input input-bordered bg-white"
         placeholder="Mail"
-        value={email}
-        onChange={(e) => setMail(e.target.value)}
       />
 
       <input
         type="password"
         className="input input-bordered bg-white"
-        placeholder="Password" // Corrected typo "Passord" to "Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Passord"
       />
 
-      <button className="text-white hover:bg-blue-700 duration-300 rounded-lg p-3 text-base focus:border-2 bg-blue-600">
+      <button className="text-white hover:bg-blue-700 duration-300 rounded-lg p-3 text-base focus:border-2 bg-blue-600" type='submit'>
         Logg inn
       </button>
     </form>
--- markedsplass/src/app/signup/Inputs.tsx
+++ markedsplass/src/app/signup/Inputs.tsx
@@ -1,21 +1,25 @@
-import React from 'react'
-import { EnvelopeIcon } from '@heroicons/react/20/solid'
+import React from 'react';
 
 const Inputs = () => {
   return (
-    <div className='flex justify-center flex-col gap-5'>
-        <div className='xl:flex-row flex-col gap-5 flex xl:gap-0 lg:justify-between'>
-          <input type="text" className="input input-bordered bg-white xl:w-64 w-auto" placeholder="Fornavn" />
-          <input type="text" className="input input-bordered bg-white xl:w-64 w-auto" placeholder="Etternavn" />
-        </div>
+    <form className="flex justify-center flex-col gap-5">
+      <input
+        type="mail"
+        className="input input-bordered bg-white"
+        placeholder="Mail"
+      />
 
-        <input type="mail" className="input input-bordered bg-white" placeholder="Mail" />
-        
-        <input type="password" className="input input-bordered bg-white" placeholder="Passord" />
+      <input
+        type="password"
+        className="input input-bordered bg-white"
+        placeholder="Passord"
+      />
 
-        <button className="text-white hover:bg-blue-700 duration-300 rounded-lg p-3 text-base focus:border-2 bg-blue-600">Logg inn</button>
-    </div>
-  )
+      <button className="text-white hover:bg-blue-700 duration-300 rounded-lg p-3 text-base focus:border-2 bg-blue-600" type='submit'>
+        Lag ny bruker
+      </button>
+    </form>
+  );
 }
 
 export default Inputs
